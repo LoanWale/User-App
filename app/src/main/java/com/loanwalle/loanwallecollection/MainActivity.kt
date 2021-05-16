60e7e321b4d99420fc3626f07e1606aeea1efe3d
deepa <deepak.majoka29@gmail.com>
2021-05-16 19:44:15 +0530

    "loanwalle"

---
+++ app/src/main/java/com/loanwalle/loanwallecollection/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.loanwalle.loanwallecollection
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loanwalle.loanwallecollection.Activity.LoanDetailActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // hello from githum
+
+        val i = Intent(this@MainActivity, LoanDetailActivity::class.java)
+        startActivity(i)
+    }
+}
