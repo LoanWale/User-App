7605f28cf89df73d5e72f29ed92cdd850e636440
deepa <deepak.majoka29@gmail.com>
2021-08-02 11:17:40 +0530

    "hello"

--- app/src/main/java/com/loanwalle/personalloan/ui/main/view/Activity/Permission_Request.kt
+++ app/src/main/java/com/loanwalle/personalloan/ui/main/view/Activity/Permission_Request.kt
@@ -8,5 +8,6 @@ class Permission_Request : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission_request)
+        // heeelllllooo
     }
 }
